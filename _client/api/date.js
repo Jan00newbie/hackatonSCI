af237fce9a1e0e37f38df7cca34475acc5143924
jan00newbie <positiveKiddo@gmail.com>
2019-11-15 23:06:53 +0100

    delete clients

--- _client/api/date.js
+++
@@ -1,7 +0,0 @@
-export const days = ['Niedziela', 'Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota']
-
-export const months = [
-    'Stycznia', 'Lutego', 'Marca', 'Kwietnia',
-    'Maja', 'Czerwca', 'Lipca', 'Sierpnia',
-    'Wrzesnia', 'Października', 'Listopada', 'Grudnia'
-]
