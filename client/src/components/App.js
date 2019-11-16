3c9057f6731dc659b6f153dedc89981c4c69f3b1
htmlprogrammer <j.maciejewski.kontakt@gmail.com>
2019-11-16 07:01:57 +0100

    client update

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,57 +1,52 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import {
-  Modal,
-  Toolbar,
-  CssBaseline,
-  Typography
-} from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import AppBarComponent from "./AppBar";
-import Home from "./Home";
-import LoginComponent from "./LoginForm";
-import RegisterComponent from "./RegisterForm";
-import About from "./About";
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Modal, Toolbar, CssBaseline, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import AppBarComponent from './AppBar'
+import Home from './Home'
+import Events from './Events'
+import About from './About'
+import LoginComponent from './LoginForm'
+import RegisterComponent from './RegisterForm'
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
     backgroundColor: theme.palette.primary.main,
-    minHeight: "100vh"
+    minHeight: '100vh'
   },
   container: {
-    width: "100%"
+    width: '100%'
   },
   modalContainer: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column"
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column'
   },
   modal: {
-    maxWidth: "500px",
-    maxHeight: "600px",
-    backgroundColor: "white",
+    maxWidth: '500px',
+    maxHeight: '600px',
+    backgroundColor: 'white',
     padding: theme.spacing(3)
   }
-}));
+}))
 
 export default () => {
-  const classes = useStyles();
-  const [modal, setModal] = useState(false);
-  
+  const classes = useStyles()
+  const [modal, setModal] = useState(false)
+
   const loginModalOpen = () => {
-    setModal('login');
-    console.log(modal)
-  };
+    setModal('login')
+  }
 
   const registerModalOpen = () => {
-    setModal('register');
-  };
+    setModal('register')
+  }
 
   const modalClose = () => {
-    setModal(false);
-  };
+    setModal(false)
+  }
 
   const modalSwap = () => {
     if (modal === 'register') {
@@ -61,21 +56,22 @@ export default () => {
       modalClose()
       registerModalOpen()
     }
-      
   }
-
-
-  
 
   return (
     <Router>
       <CssBaseline />
-      <AppBarComponent modalControls={{openLogin: loginModalOpen, openRegister: registerModalOpen}} />
+      <AppBarComponent
+        modalControls={{
+          openLogin: loginModalOpen,
+          openRegister: registerModalOpen
+        }}
+      />
       <div className={classes.root}>
         <Toolbar />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/events" render={() => <h1>Home</h1>} />
+          <Route path="/events" component={Events} />
           <Route path="/login" render={() => <h1>Home</h1>} />
           <Route path="/register" render={() => <h1>Home</h1>} />
           <Route path="/event/:id" render={() => <h1>Home</h1>} />
@@ -108,5 +104,5 @@ export default () => {
         </div>
       </Modal>
     </Router>
-  );
-};
+  )
+}
