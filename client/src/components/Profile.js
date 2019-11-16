d26ec17c9f070f3c29898c0f89a779d15a1f2232
jan00newbie <positiveKiddo@gmail.com>
2019-11-16 14:11:38 +0100

    xd

---
+++ client/src/components/Profile.js
@@ -0,0 +1,11 @@
+import React, {useContext} from 'react'
+import authContext from '../'
+
+export const Profile = ({}) => {
+    const {user} = useContext()
+    return (
+        <div>
+            
+        </div>
+    )
+}
