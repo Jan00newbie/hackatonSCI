59e43c54b80ce4c815aa4011f91e62c8179240f6
hackathon60 <hackathon60@DOM.SCI.EDU.PL>
2019-11-16 04:09:54 +0100

    xd

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,6 +5,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBarComponent from "./AppBar";
 import Home from "./Home";
 import FormComponent from "./Form"
+import About from "./About"
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -40,21 +41,6 @@ export default () => {
       <AppBarComponent handleOpen={handleOpen} />
       <div className={classes.root}>
         <Toolbar />
-<<<<<<< HEAD
-        <Grid className={classes.container} container justify="center">
-          <Grid item xs={12} md={8}>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/events" render={() => <h1>Home</h1>} />
-              <Route path="/login" render={() => <h1>Home</h1>} />
-              <Route path="/register" render={() => <h1>Home</h1>} />
-              <Route path="/event/:id" render={() => <h1>Home</h1>} />
-              <Route path="/about" component={About} />
-              <Route render={() => <h1>Page not found</h1>} />
-            </Switch>
-          </Grid>
-        </Grid>
-=======
         <div className={classes.container}>
           <Switch>
             <Route exact path="/" component={Home} />
@@ -62,11 +48,10 @@ export default () => {
             <Route path="/login" render={() => <h1>Home</h1>} />
             <Route path="/register" render={() => <h1>Home</h1>} />
             <Route path="/event/:id" render={() => <h1>Home</h1>} />
-            <Route path="/about" render={() => <h1>Home</h1>} />
+            <Route path="/about" component={About} />
             <Route render={() => <h1>Page not found</h1>} />
           </Switch>
         </div>
->>>>>>> 2f6ccb8bb0b9c0a8d8840ccf68ddcc34a2f298e5
       </div>
       <Modal
         open={open}
