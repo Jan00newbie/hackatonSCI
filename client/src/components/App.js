61bd55e4858b40479d6a3194ed7d93fd50cc5936
hackathon60 <hackathon60@DOM.SCI.EDU.PL>
2019-11-16 03:02:15 +0100

    xd

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,6 +4,7 @@ import { Toolbar, CssBaseline, Grid } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import AppBarComponent from './AppBar'
 import Home from './Home'
+import About from './About'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -34,7 +35,7 @@ export default () => {
               <Route path="/login" render={() => <h1>Home</h1>} />
               <Route path="/register" render={() => <h1>Home</h1>} />
               <Route path="/event/:id" render={() => <h1>Home</h1>} />
-              <Route path="/about" render={() => <h1>Home</h1>} />
+              <Route path="/about" component={About} />
               <Route render={() => <h1>Page not found</h1>} />
             </Switch>
           </Grid>
