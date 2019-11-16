627791c43540fc728319e91cd98805805d51e1a2
jan00newbie <positiveKiddo@gmail.com>
2019-11-16 09:04:50 +0100

    Setup auth and alert context

---
+++ client/src/context/alert/alertReducer.js
@@ -0,0 +1,17 @@
+
+import {SET_ALERT, REMOVE_ALERT} from '../types'
+
+const alertReducer = (state, change) => {
+    switch (change.type) {
+        case SET_ALERT:
+            return [...state, change.payload]
+              
+        case REMOVE_ALERT:
+            return [...state].filter(alert => alert.id !== change.payload);
+
+        default:
+            break;
+    }
+}
+
+export default alertReducer
---
+++ client/src/context/types.js
@@ -0,0 +1,3 @@
+export const LOGIN = "Login"
+export const REGISTER = "REGISTER"
+export const AUTH_FAILED = "AUTH_FAILED"
