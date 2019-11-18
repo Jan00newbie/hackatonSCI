53e58fb4e6ca7ba4bfb54bd812060235c0a28cf9
jan00newbie <positiveKiddo@gmail.com>
2019-11-18 22:09:20 +0100

    Event context  init setup

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -3,3 +3,8 @@ export const LOGOUT = "LOGOUT"
 export const LOAD_USER = "LOAD_USER"
 export const REMOVE_ALERT = "REMOVE_ALERT"
 export const SET_ALERT = "SET_ALERT"
+
+
+export const GET_EVENTS = "GET_EVENTS"
+export const UPDATE_EVENT = "UPDATE_EVENT"
+export const REMOVE_EVENT = "REMOVE_EVENT"
